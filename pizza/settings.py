d136b5041238725e7bd9c5f4bcb22da00963d9ed
Rahul rai <46862628+Rahulraifzb@users.noreply.github.com>
2020-12-08 19:46:46 +0530

    Update settings.py

--- pizza/settings.py
+++ pizza/settings.py
@@ -77,11 +77,8 @@ WSGI_APPLICATION = 'pizza.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pizza',
-        'USER': 'postgres',
-        'PASSWORD': '224133',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR/ 'db.sqlite3'
     }
 }
 
